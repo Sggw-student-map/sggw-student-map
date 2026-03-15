67897494e123476b2a27c799208d42b52053b1c5
Gwozdz1uuu <gwozdeckidaniel@gmail.com>
2026-03-15 21:42:44 +0100

    initial

---
+++ src/main/java/com/gwozdz1uuu/sggwstudentmap/SggwstudentmapApplication.java
@@ -0,0 +1,13 @@
+package com.gwozdz1uuu.sggwstudentmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SggwstudentmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SggwstudentmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gwozdz1uuu/sggwstudentmap/SggwstudentmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwozdz1uuu.sggwstudentmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SggwstudentmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
