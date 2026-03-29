3574c8ad5a9b60e227f84a228f35ed5e18aea924
jakubdabrowskii <jakubdabrowski818@gmail.com>
2026-03-29 22:01:37 +0200

    feat: add login screen and project foundations

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Map } from './map/map';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'map', component: Map },
+    { path: '**', redirectTo: 'login' }
+];
---
+++ frontend/src/app/login/login.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+  constructor(private router: Router) {}
+
+  onLogin() {
+    this.router.navigate(['/map']);
+  }
+
+}
