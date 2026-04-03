8c9108770ccf00f80b827db507c886733c7149ca
MD04-CFC <maciek.dabrowski.2004@gmail.com>
2026-04-03 13:47:23 +0200

    db improvement

--- db/init.sql
+++ db/init.sql
@@ -11,19 +11,55 @@ CREATE TABLE users (
     is_active BOOLEAN DEFAULT true
 );
 
+
 -- Tabela: places
 CREATE TABLE places (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name_ VARCHAR(255) NOT NULL,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL
 );
 
+-- Tabela znajomosci
+CREATE TABLE friendships (
+    user_id1 INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id2 INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    status_of_friendship VARCHAR(20) DEFAULT 'pending', 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id1, user_id2),
+    CONSTRAINT check_not_self CHECK (user_id1 <> user_id2)
+);
+
+
+-- Tabela eventy
+CREATE TABLE eventy (
+    id SERIAL PRIMARY KEY,
+    name_of_event VARCHAR(255) NOT NULL,
+    --id_user INT REFERENCES users(id) ON DELETE CASCADE,
+    id_place INT NOT NULL REFERENCES places(id) ON DELETE CASCADE,
+    date_of_event TIMESTAMP,
+    comment text,
+    organizer_id INT REFERENCES users(id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE users_on_events(
+    id_eventu int NOT NULLREFERENCES eventy(id) N DELETE CASCADE, 
+    id_users int not NULLREFERENCES users(id) N DELETE CASCADE
+);
+
+CREATE TABLE favortites (
+    id INT REFERENCES users(id) ON DELETE CASCADE,
+    id_place INT REFERENCES places(id) ON DELETE CASCADE,
+    PRIMARY KEY (id, id_place)
+);
+
+
 -- Tabela: reviews
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
-    place_id INT NOT NULL,
-    user_id INT NOT NULL,
+    place_id INT NOT NULL REFERENCES places(id) ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     rating INT CHECK (rating >= 1 AND rating <= 5),
     comment TEXT,
 
@@ -41,13 +77,22 @@ CREATE TABLE reviews (
 -- Tabela: routes
 CREATE TABLE routes (
     id SERIAL PRIMARY KEY,
-    place_id INT NOT NULL,
+    place_id INT NOT NULL REFERENCES places(id) ON DELETE CASCADE,
 
     CONSTRAINT fk_routes_place
         FOREIGN KEY(place_id)
         REFERENCES places(id)
         ON DELETE CASCADE
 );
+
+
+CREATE TABLE route_points (
+    route_id INT REFERENCES routes(id) ON DELETE CASCADE,
+    place_id INT REFERENCES places(id) ON DELETE CASCADE,
+    stop_order INT NOT NULL, -- kolejność zwiedzania
+    PRIMARY KEY (route_id, place_id)
+);
+
 
 -- Indeksy
 CREATE INDEX idx_users_username ON users(username);
