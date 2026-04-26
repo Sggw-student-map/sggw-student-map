c0c1e09a54a1c037e010d669127a2280db7e3660
ikmmm <igoras.kucharski@gmail.com>
2026-04-26 21:28:40 +0200

    profile new look

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -9,6 +9,7 @@ import { UserStateService } from '../core/user-state.service';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   user: CurrentUser | null = null;
