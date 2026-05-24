866f3a07e5cdc42bbe02b1364f57aed9f32fc6e1
Maciej <maciek.dabrowski.2004@gmail.com>
2026-05-24 12:30:22 +0200

    environment socrrections in atuh service

--- frontend/src/app/core/auth.service.ts
+++ frontend/src/app/core/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
-import { environment } from '../../environments/environment';
 import { TokenStorageService } from './token-storage.service';
 
 import { environment } from '../../environments/environment';
