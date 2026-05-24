d5d8100760e8cb701bb6c6cf802aec741bec1bf6
Maciej <maciek.dabrowski.2004@gmail.com>
2026-05-24 13:05:22 +0200

    auth permition for login

--- backend/src/main/java/com/gwozdz1uuu/sggwstudentmap/auth/AuthSecurityRules.java
+++ backend/src/main/java/com/gwozdz1uuu/sggwstudentmap/auth/AuthSecurityRules.java
@@ -16,6 +16,7 @@ public class AuthSecurityRules implements SecurityRules {
         registry
                 .requestMatchers(HttpMethod.GET, "/auth/confirm").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/users").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
                 .requestMatchers(HttpMethod.GET, "/auth/me").authenticated()
                 .requestMatchers(HttpMethod.GET, "/api/reviews", "/api/reviews/**").permitAll()
